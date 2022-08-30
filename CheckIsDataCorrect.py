f57b5168040a00c1a7c873d8d26b3ab951ed79eb
BITniki <shakarimovdima@ya.ru>
2022-08-30 17:05:09 +0300

    fix bugs

--- CheckIsDataCorrect.py
+++ CheckIsDataCorrect.py
@@ -13,7 +13,7 @@ def authentication(password):
 
 def isPeerIdCorrect(peerId):
     tpl = '[A-Za-z0-9]+_[0-9]+'
-    # проверка на строку, не пустоту результата парсинга и что парсинг совпадает с peerId
+    # checking for a string, for the emptiness of the parsing result and that the parsing matches the peerID
     if not isinstance(peerId, str) or len(re.findall(tpl, peerId)) == 0 or re.findall(tpl, peerId)[0] is not peerId:
         return False
     return True
@@ -21,10 +21,12 @@ def isPeerIdCorrect(peerId):
 
 def isPeerIpCorrect(peerIp):
     tpl = "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
+    # check is peerIp is string
     if not isinstance(peerIp, str):
         return False
     result = re.match(tpl, peerIp)
-    if len(result.string) < len(peerIp):
+    # check is result is not a NoneType and result match completely matches peerIp
+    if not result or result.group() is not peerIp:
         return False
     ipEnd = int(result.group(4))
     if ipEnd < 1 or ipEnd > 254:
